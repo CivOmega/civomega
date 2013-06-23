4db4d04463afa19adfd731b3aaca27e51478b560
Joe Germuska <joe@germuska.com>
2013-06-22 21:37:21 -0400

    flesh out test and implementation some more

--- tests/test_census.py
+++ tests/test_census.py
@@ -1,5 +1,5 @@
 import unittest
-
+import json
 from dataomega.census import SimpleCensusParser
 
 class TestSimpleCensusParser(unittest.TestCase):
@@ -10,4 +10,17 @@ class TestSimpleCensusParser(unittest.TestCase):
         q = 'how many Dominicans in New York?'
         match = self.parser.search(q)
         self.assertIsNotNone(match)
-    
+        
+        self.assertIsNotNone(match.as_json())
+        d = json.loads(match.as_json())
+        self.assertEquals(695158,d['04000US36']['b03001007'])
+        
+        q = "how many chileans in new york?"
+        match = self.parser.search(q)
+        self.assertIsNotNone(match)
+        
+        self.assertIsNotNone(match.as_json())
+        d = json.loads(match.as_json())
+        self.assertEquals(16764,d['04000US36']['b03001019'])
+        
+        
