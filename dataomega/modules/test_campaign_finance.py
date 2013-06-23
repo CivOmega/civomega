f43cf4edc790b5ebf2bf741dfc6a679026b0e9d0
Mike Tigas <mike@tig.as>
2013-06-22 21:57:50 -0400

    really contrived example works

--- dataomega/modules/test_campaign_finance.py
+++ dataomega/modules/test_campaign_finance.py
@@ -1,9 +1,11 @@
 import json
 import re
-import unittest
 
 from dataomega.match import Match
 from dataomega.parser import Parser
+
+from dataomega.registry import REGISTRY
+
 
 # dumb example, flat database of campaign contributions in one year
 AWESOME_DATABASE = {
@@ -37,6 +39,7 @@ class MoneyRaisedParser(Parser):
         return MoneyRaisedMatch(s, match.groupdict())
 
 
+
 class MoneyRaisedMatch(Match):
     def extract(self):
         contributors = AWESOME_DATABASE[self.data['filer']]['contributors']
@@ -59,6 +62,7 @@ class ContributorParser(Parser):
         if match is None:
             return None
         return ContributorsListMatch(s, match.groupdict())
+
 
 
 class ContributorsListMatch(Match):
@@ -85,41 +89,7 @@ class ContributorsListMatch(Match):
         return json.dumps(self.contributors) if self.contributors else None
 
 
-class TestContributorParser(unittest.TestCase):
-    def setUp(self):
-        self.matcher = ContributorParser()
-
-    def test_can_find_contributors_for_perry(self):
-        result = self.matcher.search("Who gave money to Rick Perry")
-        data = result.as_json()
-        self.assert_("Texans for Perry" in json.loads(data))
-        self.assert_("Texans who are really for Perry" in json.loads(data))
-
-    def test_can_find_contributors_for_dewhurst(self):
-        result = self.matcher.search("Who gave money to David Dewhurst")
-        data = result.as_json()
-        self.assert_("Texans for Dewhurst" in json.loads(data))
-        self.assert_("Texans who are really for Dewhurst" in json.loads(data))
-
-    def test_returns_none_for_nothing(self):
-        result = self.matcher.search("Who did not give money to Rick Perry")
-        self.assert_(result is None)
 
 
-class TestMoneyRaisedParser(unittest.TestCase):
-    def setUp(self):
-        self.matcher = MoneyRaisedParser()
-
-    def test_can_find_money_raised_for_perry(self):
-        result = self.matcher.search("How much money has Rick Perry raised")
-        data = json.loads(result.as_json())
-        self.assert_(AWESOME_DATABASE['Rick Perry']['total_money_raised'] == data["raised"])
-
-    def test_can_find_money_raised_for_dewhurst(self):
-        result = self.matcher.search("How much money has David Dewhurst raised")
-        data = json.loads(result.as_json())
-        self.assert_(AWESOME_DATABASE['David Dewhurst']['total_money_raised'] == data["raised"])
-
-    def test_returns_none_for_no_match(self):
-        result = self.matcher.search("How much money didn't Rick Perry raise")
-        self.assert_(result is None)
+REGISTRY.add_parser('money_raised', MoneyRaisedParser)
+REGISTRY.add_parser('contributors', ContributorParser)
