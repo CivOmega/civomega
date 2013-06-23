9e60043a1419ce94b7bc9c42ffa516b2b0d98208
Travis Swicegood <development@domain51.com>
2013-06-23 08:16:24 -0400

    Update to correct module location so tests run again

--- tests/test_census.py
+++ tests/test_census.py
@@ -1,30 +1,30 @@
 import unittest
 import json
-from dataomega.census import SimpleCensusParser
+from dataomega.modules.census_population import SimpleCensusParser
 
 class TestSimpleCensusParser(unittest.TestCase):
     def setUp(self):
         self.parser = SimpleCensusParser()
-    
+
     def test_a_question(self):
         q = 'how many Dominicans in New York?'
         match = self.parser.search(q)
         self.assertIsNotNone(match)
-        
+
         self.assertIsNotNone(match.as_json())
         d = json.loads(match.as_json())
         self.assertEquals('04000US36',d['place']['full_geoid'])
         self.assertEquals(695158,d['population'])
-        
+
         self.assertTrue('New York' in match.as_html())
         self.assertTrue('695158' in match.as_html())
         q = "how many chileans in new york?"
         match = self.parser.search(q)
         self.assertIsNotNone(match)
-        
+
         self.assertIsNotNone(match.as_json())
         d = json.loads(match.as_json())
         self.assertEquals('04000US36',d['place']['full_geoid'])
         self.assertEquals(16764,d['population'])
-        
-        
+
+
