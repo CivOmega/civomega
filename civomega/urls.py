737549f49a37bf77edbfc48a8e6d1edfff888ffe
Mike Tigas <mike@tig.as>
2013-12-21 17:24:39 -0500

    stub template rendering / views / etc

--- civomega/urls.py
+++ civomega/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'civomega.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='homepage'),
+    url(r'^ask$', 'civomega.codata.views.ask', name='ask'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
