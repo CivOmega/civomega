601e5f95b92fb17b4b057d797045bd3a4381f22b
Mike Tigas <mike@tig.as>
2014-01-08 16:51:54 -0500

    stub out endpoints, should think about how we want to tackle the TODOs in there

--- civomega/codata/views.py
+++ civomega/codata/views.py
@@ -1,5 +1,60 @@
 # coding=utf-8
 from django.http import HttpResponse
+import json
 
 def ask(request):
     return HttpResponse("Sorry, just a stub.", content_type="text/plain")
+
+
+def _return_jsonp(data, callback):
+    # "data" might be a dict (in which case we still need to JSON it)
+    # or a string (assumed JSON already)
+    if type(data) == dict:
+        data = json.dumps(data, indent=2)
+
+    # if callback is none, render as JSON
+    # if we have a callback, render as JSONP
+    if callback:
+        return HttpResponse("%s(%s)" % (callback, data),
+            content_type="application/json-p")
+    else:
+        return HttpResponse(data, content_type="application/json")
+
+
+def pattern_match(request):
+    query = request.GET.get('q', None)
+    callback = request.GET.get('callback', None)
+
+    data = {
+      'q': query,
+      'matches': (
+        {'id': 1, 'pattern': 'What bills are about {subject}?'}
+      )
+    }
+    return _return_jsonp(data, callback)
+
+def pattern_invoke(request):
+    module_id = request.GET.get('id', None)
+    callback = request.GET.get('callback', None)
+
+    # TODO:
+    # 1 fetch module (module_id)
+    # 2 pass `request` to module
+    # 3 module will pick up the key->value combinations associated
+    #   with that pattern, maybe? i.e. subject = request.GET.get('subject')
+    # 4 now we have args for module to do API call, return results
+    # * what do we want results JSON to look like
+
+    data = {
+      'module_id': module_id
+    }
+    return _return_jsonp(data, callback)
+
+def generic_query(request):
+    query = request.GET.get('q', None)
+    callback = request.GET.get('callback', None)
+
+    data = {
+      'q': query
+    }
+    return _return_jsonp(data, callback)
--- civomega/urls.py
+++ civomega/urls.py
@@ -5,4 +5,12 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name="index.html"), name='homepage'),
     url(r'^ask$', 'civomega.codata.views.ask', name='ask'),
 
+
+    url(r'^endpoints/pattern-match$', 'civomega.codata.views.pattern_match',
+        name='pattern-match'),
+    url(r'^endpoints/invoke$', 'civomega.codata.views.pattern_invoke',
+        name='pattern-invoke'),
+    url(r'^endpoints/generic$', 'civomega.codata.views.generic_query',
+        name='generic-query'),
+
 )
